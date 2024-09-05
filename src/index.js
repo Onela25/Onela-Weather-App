d84d58ecce941e3859f19b9ccdab6517377b9d1d
Onela25 <onelawellem0525@gmail.com>
2024-09-05 11:03:52 +0200

    Edited the Script

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function updateWeatherData(response) {
+ console.log(response.data); 
+}
+
+function searchCity(city) {
+let apiKey = "307c2540doab8f13b37004f7fdft20c1";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiURL).then(updateWeatherData);
+}
+
+
+
 function handleSearchSubmit( event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML =searchInput.value;
+    searchCity (searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
